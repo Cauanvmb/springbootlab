7aacaf55487e5bf4b020d78f4e8daa42140378bb
Cauanvmb <cauanvmb15@gmail.com>
2024-08-13 21:53:41 -0300

    config commit

---
+++ src/main/java/springbootlab4/cauan/CauanApplication.java
@@ -0,0 +1,13 @@
+package springbootlab4.cauan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CauanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CauanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbootlab4/cauan/CauanApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootlab4.cauan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CauanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
